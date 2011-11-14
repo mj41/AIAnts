dafdd96d2fd473a946e9b3cc886e1db2383dd707
Michal Jurosz <mj@mj41.cz>
2011-11-14 21:59:55 +0100

    Pseudo random bot.

--- MyBot.pm
+++ MyBot.pm
@@ -33,15 +33,52 @@ Make orders.
 =cut
 
 sub turn {
-    my $self = shift;
-    $self->SUPER::turn( @_ );
+    my ( $self, $turn_num, $turn_data ) = @_;
 
+    $self->SUPER::turn( $turn_num, $turn_data );
+
+    my $dirs = [ 'N', 'E', 'S', 'W' ];
+
+    my $used = {};
     my @orders = ();
-    foreach my $ant_data ( $self->my_ants ) {
-        my ( $ant_num, $x, $y ) = @$ant_data;
-        push @orders, [ $x, $y, 's' ];
+    my $map_obj = $self->{m};
+    my $map = $map_obj->{m};
+    my $water_bit = $map_obj->{o_bits}{water};
+    foreach my $data ( values %{$turn_data->{a}} ) {
+        my ( $x, $y, $owner ) = @$data;
+        next unless $owner == 0;
 
+        my $dir;
+        my ( $Nx, $Ny );
+        my $dir_num = 0;
+        while ( 1 ) {
+            $dir = $dirs->[ $dir_num ];
+            my ( $Dx, $Dy ) = ( 0, 0 );
+            if ( $dir eq 'N' ) {
+                $Dx--;
+            } elsif ( $dir eq 'E' ) {
+                $Dy++;
+            } elsif ( $dir eq 'S' ) {
+                $Dx++;
+            } elsif ( $dir eq 'W' ) {
+                $Dy--;
+            }
+
+            ( $Nx, $Ny ) = $map_obj->pos_plus( $x, $y, $Dx, $Dy );
+            unless ( $map->[$x][$y] & $water_bit ) {
+                last unless exists $used->{"$Nx,$Ny"};
+            }
+            $dir_num++;
+            last if $dir_num == 4;
+        }
+        if ( $dir_num == 4 ) {
+            $used->{"$x,$y"} = 1;
+        } else {
+            $used->{"$Nx,$Ny"} = 1;
+            push @orders, [ $x, $y, $dir ];
+        }
     }
+
     return @orders;
 }
 
