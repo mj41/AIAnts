7f21640aba6ca4240fe1fad081b0587deface0c0
Michal Jurosz <mj@mj41.cz>
2011-11-19 16:29:07 +0100

    MyBot.pm vs. MyBot.pm to util/run-local.sh

--- utils/run-local.sh
+++ utils/run-local.sh
@@ -18,17 +18,40 @@ fi
 
 cd aichallenge/ants || exit
 
-echo > ../../temp/game-out.txt
+# MyBot.pm vs. MyBot.pm
+if [ "$2" == "two" ]; then
 
-./playgame.py --turns $TURNS --player_seed 42 \
-  --turntime=100 --loadtime=1000 --end_wait=0.25 \
-  --verbose --log_dir game_logs -R -S -I -O -E \
-  --map_file maps/maze/maze_02p_02.map \
-  'perl ../../MyBot.pl temp/game-out.txt' \
-  "python dist/sample_bots/python/GreedyBot.py"
+    echo > ../../temp/game-out.txt
+    echo > ../../temp/game-outB.txt
 
+    ./playgame.py --turns $TURNS --player_seed 42 \
+      --turntime=100 --loadtime=1000 --end_wait=0.25 \
+      --verbose --log_dir game_logs -R -S -I -O -E \
+      --map_file maps/maze/maze_02p_02.map \
+      'perl ../../MyBot.pl temp/game-out.txt' \
+      'perl ../../MyBot.pl temp/game-outB.txt'
+
+    echo "Bot 2 error output"
+    cat game_logs/1.bot1.error
+    echo
+
+# Perl vs. Python
+else
+
+    echo > ../../temp/game-out.txt
+
+    ./playgame.py --turns $TURNS --player_seed 42 \
+      --turntime=100 --loadtime=1000 --end_wait=0.25 \
+      --verbose --log_dir game_logs -R -S -I -O -E \
+      --map_file maps/maze/maze_02p_02.map \
+      'perl ../../MyBot.pl temp/game-out.txt' \
+      'python dist/sample_bots/python/GreedyBot.py'
+fi
+
+echo "Bot 1 error output"
 cat game_logs/0.bot0.error
+echo
 
 cd ../..
+echo "Bot 1 output"
 cat temp/game-out.txt
-
