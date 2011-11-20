c849ba0dd070acf6f52f7f6d88f1f525490666c9
Michal Jurosz <mj@mj41.cz>
2011-11-20 22:41:09 +0100

    Enhance run-local.sh

--- utils/run-local.sh
+++ utils/run-local.sh
@@ -17,12 +17,15 @@ fi
 # perl -c MyBot.pl || ( echo "Failed checking syntaxt of MyBot.pl"  && exit ) 
 
 cd ../aichallenge/ants || exit
+rm game_logs/*
 
 # MyBot.pm vs. MyBot.pm
 if [ "$2" == "two" ]; then
 
     echo > ../../aiants/temp/game-out.txt
     echo > ../../aiants/temp/game-outB.txt
+    echo > game_logs/0.bot0.error
+    echo > game_logs/0.bot1.error
 
     ./playgame.py --turns $TURNS --player_seed 42 \
       --turntime=100 --loadtime=1000 --end_wait=0.25 \
@@ -31,14 +34,30 @@ if [ "$2" == "two" ]; then
       'perl ../../aiants/MyBot.pl temp/game-out.txt' \
       'perl ../../aiants/MyBot.pl temp/game-outB.txt'
 
-    echo "Bot 2 error output"
-    cat game_logs/1.bot1.error
+    cd ../../aiants/
+
+    echo "Bot 2 tail of output:"
+    tail -n 150 temp/game-outB.txt
+
+    echo "Bot 1 output:"
+    cat temp/game-out.txt
+
     echo
+    echo "Bot 2 error output:"
+    cat ../aichallenge/ants/game_logs/0.bot1.error
+    echo
+
+    echo
+    echo "Bot 1 error output:"
+    cat ../aichallenge/ants/game_logs/0.bot0.error
+    echo
+
 
 # Perl vs. Python
 else
 
     echo > ../../aiants/temp/game-out.txt
+    echo > game_logs/0.bot0.error
 
     ./playgame.py --turns $TURNS --player_seed 42 \
       --turntime=100 --loadtime=1000 --end_wait=0.25 \
@@ -46,12 +65,13 @@ else
       --map_file maps/maze/maze_02p_02.map \
       'perl ../../aiants/MyBot.pl temp/game-out.txt' \
       'python dist/sample_bots/python/GreedyBot.py'
+
+    cd ../../aiants/
+    echo "Bot 1 output:"
+    cat temp/game-out.txt
+
+    echo "Bot 1 error output:"
+    cat ../aichallenge/ants/game_logs/0.bot0.error
+    echo
 fi
 
-echo "Bot 1 error output"
-cat game_logs/0.bot0.error
-echo
-
-cd ../../aiants/
-echo "Bot 1 output"
-cat temp/game-out.txt
